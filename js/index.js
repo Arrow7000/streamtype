44f5441344d5402deb052e6a803cc59e6743af06
Aron Adler <aron@smartowldesign.com>
2016-02-01 22:17:43 +0000

    Fonts and delete timer

--- js/index.js
+++ js/index.js
@@ -3,10 +3,26 @@
 
     app.controller('mainController', function($scope, $interval) {
         $scope.timer = 15 * 60 * 1000;
+        $scope.deleteTimer = 5 * 1000;
 
         $interval(function() {
             $scope.timer -= 1000;
         }, 1000);
-    });
 
+
+        $interval(function() {
+            if ($scope.deleteTimer > 0) $scope.deleteTimer -= 10;
+        }, 10);
+
+
+
+
+
+
+
+
+
+
+
+    });
 })()
