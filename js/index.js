397c2361bfe138b39f13a5bd6048f8731a18d068
Aron Adler <aronadler@hotmail.com>
2016-02-02 12:38:34 +0000

    Fully functional version. 15 mins hardcoded.

--- js/index.js
+++ js/index.js
@@ -2,38 +2,57 @@
 	var app = angular.module('flowstate', []);
 
 	app.controller('mainController', function($scope, $interval) {
+
 		$scope.delTimeInit = 5 * 1000;
-		$scope.text = "";
-		$scope.timer = 15 * 60 * 1000;
+		$scope.timerInit = 15 * 60 * 1000;
+
+		$scope.timer = $scope.timerInit;
 		$scope.deleteTimer = $scope.delTimeInit;
-		var empty = true;
 
-		$interval(function() {
-			$scope.timer -= 1000;
-		}, 1000);
+		$scope.started = false;
+
+		$scope.ctn = angular.element(document.querySelector('#container'));
 
 
-		$interval(function() {
-			if (!empty && $scope.deleteTimer > 0) $scope.deleteTimer -= 10;
-			if (!empty && $scope.deleteTimer <= 2.5 * 1000) $scope.deleteEverything();
-			console.log("empty?", empty);
+		// $interval(function() { // Second interval counter
+		// 	$scope.timer -= 1000;
+		// }, 1000);
+
+		$interval(function() { // 1/100th second interval counter
+			if ($scope.ctn.text().length > 0 && $scope.deleteTimer > 0) { // Before timer hits 0
+				$scope.deleteTimer -= 10; // Makes deletion timer decrement 
+			}
+			if ($scope.ctn.text().length > 0 && $scope.deleteTimer <= 0 * 1000) { // When timer hits zero
+				$scope.deleteEverything();
+			}
+			if (!$scope.started) {
+				$scope.timer = $scope.timerInit;
+			} else {
+				$scope.timer -= 10;
+			}
+
+
 		}, 10);
 
 
-		document.addEventListener("keydown", keydown, false);
+		document.addEventListener("keyup", keyup, false);
 
-		function keydown(e) {
-			empty = false;
+		function keyup(e) {
+			console.log("Key up!");
+			console.log("$scope.started", $scope.started);
+			console.log("$scope.ctn.text().length", $scope.ctn.text().length);
+
+
 			$scope.deleteTimer = $scope.delTimeInit;
-			console.log($scope.text);
+			if ($scope.ctn.text().length > 0) $scope.started = true;
 		}
 
 
-		var container = angular.element(document.querySelector('#container'));
+
 		$scope.deleteEverything = function() {
-			empty = true;
-			container.empty();
+			$scope.ctn.empty();
 			$scope.deleteTimer = $scope.delTimeInit;
+			$scope.started = false;
 			console.log("Timer ran out!");
 		};
 
