3de4341e87a1ec3f05d50dcd7bda0d55fe213761
Aron Adler <aronadler@hotmail.com>
2016-02-02 11:21:24 +0000

    Rudimentary functionality complete.

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
 # Flowstate
+A clone of the Flowstate app for OSX and iOS. But for everyone with a browser. And free. 
+## Try it out
+Try it on [aronadler.com/flowstate](http://aronadler.com/flowstate).
--- js/index.js
+++ js/index.js
@@ -1,28 +1,44 @@
 (function() {
-    var app = angular.module('flowstate', []);
+	var app = angular.module('flowstate', []);
 
-    app.controller('mainController', function($scope, $interval) {
-        $scope.timer = 15 * 60 * 1000;
-        $scope.deleteTimer = 5 * 1000;
+	app.controller('mainController', function($scope, $interval) {
+		$scope.delTimeInit = 5 * 1000;
+		$scope.text = "";
+		$scope.timer = 15 * 60 * 1000;
+		$scope.deleteTimer = $scope.delTimeInit;
+		var empty = true;
 
-        $interval(function() {
-            $scope.timer -= 1000;
-        }, 1000);
-
-
-        $interval(function() {
-            if ($scope.deleteTimer > 0) $scope.deleteTimer -= 10;
-        }, 10);
+		$interval(function() {
+			$scope.timer -= 1000;
+		}, 1000);
 
 
+		$interval(function() {
+			if (!empty && $scope.deleteTimer > 0) $scope.deleteTimer -= 10;
+			if (!empty && $scope.deleteTimer <= 2.5 * 1000) $scope.deleteEverything();
+			console.log("empty?", empty);
+		}, 10);
+
+
+		document.addEventListener("keydown", keydown, false);
+
+		function keydown(e) {
+			empty = false;
+			$scope.deleteTimer = $scope.delTimeInit;
+			console.log($scope.text);
+		}
+
+
+		var container = angular.element(document.querySelector('#container'));
+		$scope.deleteEverything = function() {
+			empty = true;
+			container.empty();
+			$scope.deleteTimer = $scope.delTimeInit;
+			console.log("Timer ran out!");
+		};
 
 
 
 
-
-
-
-
-
-    });
+	});
 })()
