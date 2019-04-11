5a8481190d20ff3c10ac4a04215a32693d2c5e7a
Aron <aronadler@hotmail.com>
2019-04-11 20:37:05 +0100

    Spacing change

--- functions/download.js
+++ functions/download.js
@@ -1,8 +1,10 @@
 exports.handler = function(event, context, callback) {
     const { file } = event.queryStringParameters;
+
     const response = {
         statusCode: 200,
         body: file
     };
+
     callback(response);
 };
